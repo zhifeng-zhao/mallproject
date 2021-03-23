8b2caa06ffd9f90696bd173981bd9c49659e5d6f
zhaozhifeng <616089013@qq.com>
2021-03-23 22:27:19 +0800

    feat: gateway

---
+++ mallproject-gateway/src/test/java/com/zzf/mallproject/gateway/MallprojectGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzf.mallproject.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallprojectGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
